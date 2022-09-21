c5884b86d5a1b37d733abec2e93b32c94138e0d3
manub <manubittu1986@gmail.com>
2022-09-22 00:44:54 +0530

    Modified session and security config

---
+++ src/main/java/com/manish/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.manish.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+}
