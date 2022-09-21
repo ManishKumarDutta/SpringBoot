e93f72fbc42eed2de99e5c887b5c4a45f00794b5
manub <manubittu1986@gmail.com>
2022-09-21 22:02:39 +0530

    Security configuration

--- src/main/java/com/manish/config/BasicSecurityConfiguration.java
+++ src/main/java/com/manish/config/BasicSecurityConfiguration.java
@@ -1,8 +1,22 @@
 package com.manish.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class BasicSecurityConfiguration {
 
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/login").permitAll()
+		.antMatchers("/student/**").hasAnyAuthority(null)
+		
+	}
+	
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		
+	}
 }
